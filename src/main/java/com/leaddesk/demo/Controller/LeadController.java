629759548ba78101cbb19427d2ada6e7f18dfe8b
Srimathi <saravanansrimathi2005@gmail.com>
2026-07-27 01:19:01 +0530

    fix cors for frontend

--- src/main/java/com/leaddesk/demo/Controller/LeadController.java
+++ src/main/java/com/leaddesk/demo/Controller/LeadController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/leads")
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "*")
 
 public class LeadController {
 
